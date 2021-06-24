1a5cc00aa1a35bfb8a922d32a396703b1630a746
Napo023 <57601124+Napo023@users.noreply.github.com>
2021-06-23 19:11:32 -0500

    Add files via upload

---
+++ js/datatables-simple-demo.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    
+    const datatablesSimple = document.getElementById('datatablesSimple');
+    if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+    }
+    
+});
+
+
+
